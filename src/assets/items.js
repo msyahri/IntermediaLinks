1b9590f3dba7a66769fd271ac88c3723564c247c
msyahri <m.syahri@outlook.com>
2021-12-14 09:15:08 +0700

    Ready to Deploy

---
+++ src/assets/items.js
@@ -0,0 +1,85 @@
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import appstore from '../assets/playstore.png'
+import youtube from '../assets/youtube.jpeg'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import telegram from '../assets/telegram.png'
+import twitter from '../assets/twitter.png'
+import iitc from '../assets/iitc.png'
+import evoting from '../assets/evoting.png'
+
+const items = [{
+    "title": "Website",
+    "subtitle": "Buka website UKM INTERMEDIA",
+    "image": website,
+    "link": "https://www.intermediaamikom.org" //your personal website or portfolio  link
+},
+               
+/*{
+    "title": "GitHub",
+    "subtitle": "@devgossips | 🏠 of my open-source projects",
+    "image": github,
+    "link": "https://github.com/devgossips" //Github Profile link
+},
+*/
+               
+{
+    "title": "Instagram",
+    "subtitle": "@hintermedia_amikom | Update Terkini & Info INTERMEDIA",
+    "image": instagram,
+    "link": "https://instagram.com/intermedia_amikompwt" //instagram profile link 
+},
+               
+/*               
+{
+    "title": "Twitter",
+    "subtitle": "@heysagnik | Don't forget to follow me 😉",
+    "image": twitter,
+    "link": "https://twitter.com/heysagnik"// twitter profile link 
+},
+{
+    "title": "Apps",
+    "subtitle": "Hub of my awesome 🔥 Apps",
+    "image": appstore,
+    "link": "#"//
+},
+*/
+               
+{
+    "title": "YouTube",
+    "subtitle": "UKM INTERMEDIA | Official Youtube Channel",
+    "image": youtube,
+    "link": "https://www.youtube.com/UKMINTERMEDIA" //youtube channel link 
+},
+{
+    "title": "IITC",
+    "subtitle": "Buka Website IITC",
+    "image": iitc,
+    "link": "https://www.iitc.intermediaamikom.org" //youtube channel link 
+},               
+{
+    "title": "E-Voting",
+    "subtitle": "E-Voting Platform",
+    "image": evoting,
+    "link": "http://evoting.intermediaamikom.org" //Evoting link 
+}
+
+/*               
+{
+    "title": "Dribbble",
+    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
+    "image": dribbble,
+    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+},
+{
+    "title": "Telegram",
+    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
+    "image": telegram,
+    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
+}
+*/
+
+]
+
+export default items
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../css/skeleton.css'
+import '../css/normalize.css'
+import '../css/components.css'
+
+function Footer() {
+    return (
+        <div className="Footer container">
+        <p>INTERMEDIA <span class="heart">❤️</span> <a href={"https://intermediaamikom.org"}>#WeAreFamily!</a></p>
+        <p> <a href={"https://amikompurwokerto.ac.id"}>Universitas Amikom Purwokerto</a></p>
+        <p>React || Sagnik Sahoo</p>
+        </div>
+    )
+}
+
+export default Footer
